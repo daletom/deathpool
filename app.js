55a1b4b8b6f9abeee7bdea3587fbe027fff2ec41
Tom Dale <tom@imgix.com>
2019-02-06 23:18:27 -0800

    trying multiple vue instances with button

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const app = new Vue({
-	el: '#app',
-	data: {
-		name: 'Tom'
-	}
-}
-)
+ Vue.component('death', {
+ 	template: '<p>This person will {{ deathChoice }}. <button v-on:click="changeDeath">Live or Die?</button></p>',
+ 	data: function(){
+ 		return {
+ 			deathChoice: 'live'
+ 		}
+ 	},
+ 	methods: {
+ 		changedeath: function(){
+ 			this.deathChoice = 'dead'
+ 		}
+ 	}
+ })
+
+ new Vue({
+	el: '#app-jon'
+});
+
+  new Vue({
+	el: '#app-sansa'
+});
