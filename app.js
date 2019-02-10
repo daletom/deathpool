192308cc12a780667e0dc46dd61f4db1647d20bf
Tom Dale <tom@imgix.com>
2019-02-09 21:10:43 -0800

    updates

--- app.js
+++ app.js
@@ -27,29 +27,85 @@
 });
 
     new Vue({
-	el: '#app-aya',
+	el: '#app-arya',
 	data: {
-		name: 'Aya Stark'
+		name: 'Arya Stark', avatar: 'https://tom.imgix.net/aryastark.jpg?w=300&h=33&fit=facearea&facepad=1.7&auto=format,compress&mask=ellipse'
+	}
+});
+
+        new Vue({
+	el: '#app-bran',
+	data: {
+		name: 'Bran Stark', avatar: 'https://tom.imgix.net/branstark.jpg?w=300&h=33&fit=facearea&facepad=1.7&auto=format,compress&mask=ellipse'
 	}
 });
 
      new Vue({
-	el: '#app-jamie',
+	el: '#app-jaime',
 	data: {
-		name: 'Jamie Lannister', avatar: 'https://tom.imgix.net/jonsnow.jpg?w=300&h=300&fit=facearea&facepad=1.7&auto=format,compress&mask=ellipse'
+		name: 'Jaime Lannister', avatar: 'https://tom.imgix.net/jaimelannister.png?w=300&h=300&fit=facearea&facepad=1.7&auto=format,compress&mask=ellipse'
 	}
 });
 
   new Vue({
-	el: '#app-cercie',
+	el: '#app-cersie',
 	data: {
-		name: 'Cercie Lannister', avatar: 'https://tom.imgix.net/sansastark.jpg?w=300&h=300&fit=facearea&facepad=1.7&auto=format,compress&mask=ellipse'
+		name: 'Cersie Lannister', avatar: 'https://tom.imgix.net/cersielannister.jpg?w=300&h=300&fit=facearea&facepad=1.7&auto=format,compress&mask=ellipse'
+	}
+});
+
+    new Vue({
+	el: '#app-tyrion',
+	data: {
+		name: 'Tyrion Lannister', avatar: 'https://tom.imgix.net/tyrionlannister.jpg?w=300&h=300&fit=facearea&facepad=1.7&auto=format,compress&mask=ellipse'
 	}
 });
 
     new Vue({
 	el: '#app-mountain',
 	data: {
-		name: 'The Mountain'
+		name: 'The Mountain', avatar: 'https://tom.imgix.net/mountain.jpg?w=300&h=300&fit=facearea&facepad=1.7&auto=format,compress&mask=ellipse'
+	}
+});
+
+    new Vue({
+	el: '#app-daenerys',
+	data: {
+		name: 'Daenerys Targaryen', avatar: 'https://tom.imgix.net/daenerystargaryen.jpg?w=300&h=300&fit=facearea&facepad=1.7&auto=format,compress&mask=ellipse'
+	}
+});
+
+    new Vue({
+	el: '#app-missandei',
+	data: {
+		name: 'Missandei', avatar: 'https://tom.imgix.net/missandei.jpg?w=300&h=300&fit=facearea&facepad=1.7&auto=format,compress&mask=ellipse'
+	}
+});
+
+    new Vue({
+	el: '#app-greyworm',
+	data: {
+		name: 'Grey Worm', avatar: 'https://tom.imgix.net/greyworm.jpg?w=300&h=300&fit=facearea&facepad=1.7&auto=format,compress&mask=ellipse'
+	}
+});
+
+    new Vue({
+	el: '#app-euron',
+	data: {
+		name: 'Euron Greyjoy', avatar: 'https://tom.imgix.net/eurongreyjoy.png?w=300&h=300&fit=facearea&facepad=1.7&auto=format,compress&mask=ellipse'
+	}
+});
+
+    new Vue({
+	el: '#app-yara',
+	data: {
+		name: 'Yara Greyjoy', avatar: 'https://tom.imgix.net/yaragreyjoy.jpg?w=300&h=300&fit=facearea&facepad=1.7&auto=format,compress&mask=ellipse'
+	}
+});
+
+    new Vue({
+	el: '#app-theon',
+	data: {
+		name: 'Theon Greyjoy', avatar: 'https://tom.imgix.net/theongreyjoy.jpg?w=300&h=300&fit=facearea&facepad=1.7&auto=format,compress&mask=ellipse'
 	}
 });
